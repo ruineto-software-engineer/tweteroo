e077adeb29c6c69c9b0a3fde93ddc8470a677756
Rui Neto <ruineto11@gmail.com>
2022-04-12 00:07:40 -0300

    Prepare for deploy

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let _username = "";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = process.env.BASE_URL;
 
 function signUp() {
   const username = document.querySelector("#username").value;
